4aca7bba9c45508694130212fbe08255309ebf8a
romanserk <33842781+romanserk@users.noreply.github.com>
2019-02-18 00:49:06 +0200

    gameplay improvement

--- main.js
+++ main.js
@@ -1,39 +1,22 @@
 var usser = {
     usserMoves: [null, null, null, null, null, null, null, null, null, null],
     XorO: '',
+    color: '',
 
     addMove: function (position) {
         usser.usserMoves[position] = position;
-        console.log(gameStatus.totalMoves);
-        if (gameStatus.checkWinner(usser.usserMoves)) {
-            alert('You wone');
-        }
-        if (++gameStatus.totalMoves > 8) {
-            alert('It is a draw');
-        } else {
-            gameStatus.makeComputerMove();
-        }
-
     },
+
 }
 
 var computer = {
     computerMoves: [null, null, null, null, null, null, null, null, null, null],
     XorO: '',
+    color: '',
 
     addMove: function (position) {
         computer.computerMoves[position] = position;
-        console.log(gameStatus.totalMoves);
-        if (gameStatus.checkWinner(computer.computerMoves)) {
-            alert('Computer wone');
-        }
-        if (++gameStatus.totalMoves > 8) {
-            alert('It is a draw');
-        }
-
-
     },
-
 
     generateRndomMove: function () {
         var movePosition = Math.floor(Math.random() * 9) + 1;
@@ -57,27 +40,37 @@ var gameStatus = {
     },
 
     makeUsserMove: function (element) {
+
         var boxNum = parseInt(element.id);
         if (gameStatus.isValidTurn(boxNum)) {
-            element.innerHTML = usser.XorO;
             gameStatus.filledPositions[boxNum] = boxNum;
+            usser.addMove(boxNum);
             setTimeout(function () {
-                usser.addMove(boxNum);
-            }, 200);
-            
+                if(!gameStatus.checkWinner(usser.usserMoves , 'You')){
+                    gameStatus.makeComputerMove();
+                }
+               
+            }, 100);
+            view.show();
         }
     },
 
     makeComputerMove: function () {
+
         var computerMove = computer.generateRndomMove();
-        var com = document.getElementById(computerMove);
-        com.innerHTML = computer.XorO;
         gameStatus.filledPositions[computerMove] = computerMove;
         computer.addMove(computerMove);
+        setTimeout(function () {
+            gameStatus.checkWinner(computer.computerMoves , 'Computer')
+        }, 100);
+
+        view.show();
+
     },
 
-    checkWinner: function (movesToCheck) {
+    checkWinner: function (movesToCheck , whoToCheck) {
         var haveWinner = false;
+        var winnerHeader = document.getElementById('winner');
         var winningsArr = [
             [1, 2, 3],
             [4, 5, 6],
@@ -88,7 +81,6 @@ var gameStatus = {
             [1, 5, 9],
             [3, 5, 7]
         ];
-        console.log(movesToCheck);
         for (var i = 0; i < winningsArr.length; i++) {
             if (movesToCheck[winningsArr[i][0]] === winningsArr[i][0] &&
                 movesToCheck[winningsArr[i][1]] === winningsArr[i][1] &&
@@ -96,9 +88,45 @@ var gameStatus = {
                 haveWinner = true;
             }
         }
+
+        if (haveWinner) {
+            gameStatus.resetGame();
+            winnerHeader.innerHTML = whoToCheck + 'Won';
+            view.show();
+        }
+        else if (++gameStatus.totalMoves > 8) {
+            gameStatus.resetGame();
+            winnerHeader.innerHTML = 'It is a draw';
+            view.show();
+        }
+
         return haveWinner;
     },
 
+    resetGame: function () {
+        gameStatus.firstMove = true;
+        gameStatus.filledPositions = [];
+        gameStatus.totalMoves = 0;
+        usser.XorO = '';
+        computer.XorO = '';
+        usser.color = '';
+        computer.color = '';
+        var theBoard = document.getElementById('gameGrid');
+        var chooseType = document.getElementById('chooseType');
+
+        var toClean;
+
+        for (var i = 1; i <= 9; i++) {
+            usser.usserMoves[i] = null;
+            computer.computerMoves[i] = null;
+            toClean = document.getElementById(i);
+            toClean.innerHTML = '';
+        }
+
+        theBoard.style.display = 'none';
+        chooseType.style.display = 'block';
+
+    }
 }
 
 var view = {
@@ -109,11 +137,27 @@ var view = {
         usser.XorO = element.innerHTML;
         if (element.innerHTML === 'X') {
             computer.XorO = 'O';
+            computer.color = '#5bc0de';
+            usser.color = '#ac2925';
         } else {
             computer.XorO = 'X';
+            computer.color = '#ac2925';
+            usser.color = '#5bc0de';
         }
     },
 
+    show: function () {
+        for (var i = 1; i <= 9; i++) {
+            var position = document.getElementById(i);
+            if (computer.computerMoves[i] !== null) {
+                position.innerHTML = computer.XorO;
+                position.style.color = computer.color;
+            } else if (usser.usserMoves[i] !== null) {
+                position.innerHTML = usser.XorO;
+                position.style.color = usser.color;
+            }
+        }
+    },
 
     setUpListeners: function () {
         var gameGrid = document.getElementById('gameGrid');
@@ -121,8 +165,9 @@ var view = {
 
         gameGrid.addEventListener('click', function () {
             var elementClicked = event.target;
-            gameStatus.makeUsserMove(elementClicked);
-
+            if (event.target !== gameGrid) {
+                    gameStatus.makeUsserMove(elementClicked);
+            }
         });
 
         chooseType.addEventListener('click', function (event) {
@@ -133,4 +178,3 @@ var view = {
     }
 
 };
-view.setUpListeners();
